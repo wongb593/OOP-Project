deb1d8de3688f89f5a26ad7c5b976efc0271b787
wongb593 <150747382+wongb593@users.noreply.github.com>
2025-01-27 09:10:21 -0800

    Apparently my files have not been committing
    I am now trying to fix commit, I also have added the variable I wanted and just need to properly implement them into the code I want

--- Pizza Maker/src/Order.java
+++ Pizza Maker/src/Order.java
@@ -2,29 +2,73 @@ import java.util.Scanner; // Import the Scanner class
 
  class Order {
   public static void main(String[] args) {
-    String V, M, S, C, sum;
-    String name;
+    int C, calories, price, sum;
+    String name, pizza;
     Scanner myObj = new Scanner(System.in); // Create a Scanner object
 
     System.out.println("Type your Name:");
     name = myObj.nextLine();
 
-    System.out.println("Type your Vegetable:");
-    V = myObj.nextLine(); // Read user input
+    System.out.println("Type your Pizza:");
+    pizza = myObj.nextLine(); // Read user input
 
-    System.out.println("Type you Meat:");
-    M = myObj.nextLine(); // Read user input
-
-    System.out.println("Type your Dressing:");
-    S = myObj.nextLine(); // Read user input
-
-    System.out.println("Type your Cheese:");
-    C = myObj.nextLine(); // Read user input
+    System.out.println("Type your crust #:");
+    C = myObj.nextInt(); // Read user input
 
    // sum = V + M + S + C;  // Calculate the Calories of Vegetables, Meat, Sauce, and Cheese
    // System.out.println("Sum is: " + sum); // Print the sum
 
-    System.out.println("Vegetable:" + V);
+    sum = 0;
+    calories = 0;
+    price = 0;
+
+   if (pizza=="Veggie") {
+    System.out.println(sum = calories + 1);
+   }
+
+   if (pizza=="Meat lover") {
+    System.out.println(sum = calories + 2);
+   } 
+
+   if (pizza=="Cheese") {
+    System.out.println(sum = calories + 3);
+   }
+
+   if (pizza=="Pepporonni") {
+    
+    System.out.println(sum = calories + 4);
+   }
+
+   if (C==1) {
+    System.out.println(sum = calories + 1);
+   }
+
+   if (C==2) {
+    System.out.println(sum = calories + 2);
+   }
+
+   if (C==3) {
+    System.out.println(sum = calories + 3);
+   }
+
+   System.out.println("Order for " + name + ", one " + pizza + " pizza (calories: " + calories + ")" );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     myObj.close();
