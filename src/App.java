66673c61da8dc632668784391a1bd29e274643c6
wongb593 <150747382+wongb593@users.noreply.github.com>
2025-01-30 11:36:10 -0800

    Update App.java
    This is my final commit, I spent a couple hours and coding this using code.org. I am copy and pasting what I made from code.org to GitHub, finished with comments and fixed variables, including switches.

--- src/App.java
+++ src/App.java
@@ -1,40 +1,42 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.ArrayList; // Imports Array
+import java.util.Scanner; // Imports Scanner
 
 public class App {
     
     public static void main(String[] args) {
 
-        String name, pizza;
-        Scanner myObj = new Scanner(System.in); // Create a Scanner object
+        // Name variable and pizza variable
+        String name, pizza; 
 
-        System.out.println("Type your Name: ");
-        name = myObj.nextLine();
+        // Create a Scanner object
+        Scanner myObj = new Scanner(System.in); 
 
-        ArrayList<String> Ingredients= new ArrayList<String>();
+        System.out.println("Type your Name: "); // Reads user input in format of string
+        name = myObj.nextLine(); 
+      
+        // Display the list of pizza using ARRAY, not system print
+        ArrayList<String> Ingredients = new ArrayList<String>(); 
         Ingredients.add(" ");
-        Ingredients.add("Types of Pizza");
+        Ingredients.add("Types of Pizza:");
         Ingredients.add("Veggie");
         Ingredients.add("Meatlover");
         Ingredients.add("Cheese");
-        Ingredients.add("Pepporonii");
+        Ingredients.add("Pepperoni");
         Ingredients.add(" ");
         for (String i : Ingredients) {
-          System.out.println(i);
+            System.out.println(i);
         }
 
-        System.out.println("Type your Pizza: ");
-        pizza = myObj.nextLine(); // Read user input
+        System.out.println("Type your Pizza (Type with proper caps): ");
+        pizza = myObj.nextLine(); // Reads user input in format of string
 
-        myObj.close();
+        // Closes Obj so it doesn't read anymore
+        myObj.close(); 
 
-        Order ben = new Order();
+        // Create an Order object with the pizza type
+        Order pizzaben = new Order(pizza);
 
-        System.out.println("\n" + "order for " + name + "\n" + ben);
-
-
-
-
-
+        // Prints the name, then the attributes for pizzaben based on user input
+        System.out.println("\nOrder for " + name + "\n" + pizzaben); 
     }
 }
